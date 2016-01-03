2b0b981ac8c91de607da059739d4b3cfe9a3d7b7
hurumeki <hurumeki1987@gmail.com>
2016-01-03 12:58:12 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Mascot Ops Assets Pronama Chan MMD
+mascot-ops用のプロ生ちゃんMMDデータセットです。
+
+## License
+以下のものを使わせていただいています。
+
+* プロ生ちゃん (MMD model, icon)  
+  [ガイドライン](http://pronama.azurewebsites.net/pronama/guideline/)
+
+* Motion  
+  [https://bowlroll.net/file/9541](https://bowlroll.net/file/9541)
---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  icon: ['node_modules', 'mascot-ops-assets-pronama-chan-mmd', 'icon', 'icon.png'].join("/"),
+  model: ['node_modules', 'mascot-ops-assets-pronama-chan-mmd', 'model', 'プロ生ちゃん.pmd'].join("/"),
+  motions: [
+    ['node_modules', 'mascot-ops-assets-pronama-chan-mmd', 'motion', 'stand.vmd'].join("/")
+  ],
+  texturePath: ['.', 'node_modules', 'mascot-ops-assets-pronama-chan-mmd', 'default', ''].join("/")
+}
